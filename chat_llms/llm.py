2869a36428c9d3215eee0f60e90b6238478c9610
Yejoon Jung <85459504+joon2730@users.noreply.github.com>
2025-04-19 14:35:08 +0900

    add test

--- chat_llms/llm.py
+++ chat_llms/llm.py
@@ -4,14 +4,21 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
-# from langchain.memory import ConversationBufferMemory
-# from langchain.memory import ConversationSummaryMemory
 
 from chat_llms.prompt import CHAT_PROMPT
 from chat_llms.config import Config
 from chat_llms.utils import options
 
+from langgraph.graph import START, END, StateGraph
+from langchain_core.runnables import RunnableLambda
+from typing_extensions import TypedDict
+
 import random
+
+class State(TypedDict):
+    session_id: str
+    user_message: str
+    response: str
 
 class ChatLLM:
     def __init__(self):
@@ -33,14 +40,44 @@ class ChatLLM:
             history_messages_key="history",
         )
 
+        self.graph = self.build_graph()
+    
+    # ==== private ====
     def get_session_history(self, session_id: str) -> BaseChatMessageHistory:
         if session_id not in self.store:
             self.store[session_id] = ChatMessageHistory()
         return self.store[session_id]
 
-    def generate_response(self, user_message, session_id):
+    def respond_node(self, state: State) -> State:
+        session_id = state["session_id"]
+        user_message = state["user_message"]
+
         print(f"Request from Session ID: {session_id}")
-        return self.with_history.invoke(
+        response = self.with_history.invoke(            # Invoke the LLM with history
             {"input": user_message},
             config={"configurable": {"session_id": session_id}},
         ).content
+
+        state["response"] = response    # Set the response in the state
+        return state
+
+    def build_graph(self):
+        graph_builder = StateGraph(State) # Create a graph builder
+
+        graph_builder.add_node(self.respond_node)  # Add nodes
+
+        graph_builder.add_edge(START, "respond_node")   # Add edges
+        graph_builder.add_edge("respond_node", END)
+
+        runnable = graph_builder.compile() # Compile the graph into a runnable
+        return runnable
+
+    # ==== public ====
+    def generate_response(self, user_message: str, session_id: str) -> str:
+        state = State(                      # Create a new state
+            session_id=session_id,
+            user_message=user_message,
+            response="",
+        )
+        result = self.graph.invoke(state)   # Invoke the graph with the state
+        return result["response"]           # return the response
--- chat_llms/prompt.py
+++ chat_llms/prompt.py
@@ -6,13 +6,51 @@ You are a human chatting casually in an online chat.
 Be natural and talk short and concise.
 """
 
-prompt = PromptTemplate(
-    input_variables=["current_time"],
-    template=_chat_instruction,
+# prompt = PromptTemplate(
+#     input_variables=["current_time"],
+#     template=_chat_instruction,
+# )
+
+chat_history_test = [
+    ("user", "hi"),
+    ("assistant", "hi!"),
+    ("user", "where are u from?"),
+    ("assistant", "I am from japan! wbu?"),
+    ("user", "i am from korea, nice to meet u"),
+    ("assistant", "yeah nice to meet u too"),
+    ("user", "are you m or f?"),
+    ("assistant", "I am female"),
+    ("user", "oh i am female too, where do u live?"),
+    ("assistant", "i live in tokyo"),
+    ("user", "i live in seoul. what do u like to do"),
+    ("assistant", "i like listening to music. especially classical!"),
+    ("user", "oh what ur fav artists?"),
+    ("assistant", "chopin always!"),
+    ("user", "oh i like debussy"),
+    ("assistant", "debussy is also great"),
+    ("user", "do yo have a boyfriend?"),
+    ("assistant", "no im single now. broke up yesterday"),
+    ("user", "oh.. what happened?"),
+    ("assistant", "my ex cheated on me"),
+]
+
+memory_summerization_prompt = """
+Based on the following chat history, summarize info of both people. Be concise and structured with point form
+"""
+
+long_term_memory_test = """
+
+"""
+
+CHAT_PROMPT = ChatPromptTemplate.from_messages(
+    [("system", _chat_instruction)] +
+    chat_history_test +
+    [MessagesPlaceholder(variable_name="history"),
+    ("user", "{input}")]
 )
 
-CHAT_PROMPT = ChatPromptTemplate.from_messages([
-    ("system", prompt.format(current_time=datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M"))),
-    MessagesPlaceholder(variable_name="history"),
-    ("human", "{input}"),
-])
+# CHAT_PROMPT = ChatPromptTemplate.from_messages([
+#     ("system", _chat_instruction).
+#     MessagesPlaceholder(variable_name="history"),
+#     ("user", "{input}")
+# ])
